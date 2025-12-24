329e0fba73b23138829efa980a27ffae0990d3ba
Giridhar Sai Chikkala <giridharsaichikkala@gmail.com>
2025-12-24 14:04:10 +0530

    added services also fixed bugs

--- src/main/java/com/example/toDo/service/TodoService.java
+++ src/main/java/com/example/toDo/service/TodoService.java
@@ -1,21 +1,38 @@
 package com.example.toDo.service;
 
 import com.example.toDo.model.Todo;
+import com.example.toDo.repository.TodoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TodoService {
+
+    @Autowired
+    private TodoRepo repo;
+
     public List<Todo> getTodos() {
+        return repo.findAll();
     }
 
     public void addProduct(Todo todo) {
+        repo.save(todo);
     }
 
     public Todo getTodoById(int id) {
+        return repo.findById(id).orElse(new Todo());
     }
 
     public void updateProduct(Todo todo) {
+        List<Todo> all = getTodos();
+         Todo tt = all.stream()
+                .filter(t -> t.getId() == todo.getId())
+                 .findFirst()
+                 .orElseThrow(() -> new RuntimeException("Todo not found"));
+
+         repo.save(tt);
+
     }
 }
