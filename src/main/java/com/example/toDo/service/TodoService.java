8b0c65dccf1060f7e870314dc158298e1d491831
HemaYeddula <hemayeddula4@gmail.com>
2025-12-24 13:24:59 +0530

    Update todo controller, service, and repository

--- src/main/java/com/example/toDo/service/TodoService.java
+++ src/main/java/com/example/toDo/service/TodoService.java
@@ -1,4 +1,21 @@
 package com.example.toDo.service;
 
+import com.example.toDo.model.Todo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TodoService {
+    public List<Todo> getTodos() {
+    }
+
+    public void addProduct(Todo todo) {
+    }
+
+    public Todo getTodoById(int id) {
+    }
+
+    public void updateProduct(Todo todo) {
+    }
 }
